6f0c2a6c35b90818d372d0cecacb84d31ca6c514
Gabriel Peres <55238431+GabePeres@users.noreply.github.com>
2023-03-22 09:50:04 -0300

    Add files via upload

---
+++ ReadXML.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+import glob
+
+arquivos  = glob.glob("*.xml")
+textoXML  = open('arqTextoXML.txt','w')
+
+for arq in arquivos:
+  tree = ET.parse(arq)
+  root = tree.getroot()
+  
+  for child in root.iter():
+    tag = child.tag.split('}')[-1]        
+    if len(child) == 0:        
+        textoXML.write(str(child.text)+'|')
+  textoXML.write('\n')
+
+textoXML.close()
+#caso os arquivos XMLs forem diferentes poderá ocorrer das informações ficarem em colunas diferentes.
